6f5e1f99b45c015af5dba501308d4d90c33f3598
Hades1253 <96542998+Hades1253@users.noreply.github.com>
2022-11-06 19:19:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload() {
+    
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.om('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    image(vieo, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+        {
+            noseX = results[0].pose.nose.x-15;
+            noseY = results[0].pose.nose.y-15;
+        }
+}
+
